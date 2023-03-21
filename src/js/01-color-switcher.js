28fe1109c3791a8900e735176ede6438ecd56d06
Vladyslava-Baltsat <VladislavaBaltsat@gmail.com>
2023-03-21 17:44:17 +0200

    task-1-code

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,38 @@
+const startBtn = document.querySelector('[data-start]');
+const stopBtn = document.querySelector('[data-stop]');
+const bodyEL = document.querySelector('body');
 
+let intervalForColor = null;
+let colorStart = false;
+stopBtn.disabled = true;
+
+startBtn.addEventListener('click', onStartBtnClick);
+stopBtn.addEventListener('click', onStopBtnClick);
+
+function onStartBtnClick() {
+  changeColor();
+  startBtn.disabled = true;
+  stopBtn.disabled = false;
+}
+
+function changeColor() {
+  if (colorStart) {
+    return;
+  }
+  colorStart = true;
+  intervalForColor = setInterval(() => {
+    bodyEL.style.backgroundColor = getRandomHexColor();
+  }, 1000);
+}
+
+function onStopBtnClick() {
+  startBtn.disabled = false;
+  stopBtn.disabled = true;
+  colorStart = false;
+  clearInterval(intervalForColor);
+  //   bodyEL.style.backgroundColor = '#ffffff';
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
+}
